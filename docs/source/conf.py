c76434c23caf3a5407707ef50066a1fea7c058c4
Alex S <116145134+alexsem1337@users.noreply.github.com>
2022-10-19 11:53:19 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CS101 Bootcamp'
+copyright = '2022'
+author = ''
 
 release = '0.1'
 version = '0.1.0'
